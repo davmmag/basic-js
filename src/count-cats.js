4cd30ec31c212adc632531c4ccadd60b7d132395
davmmag <goodofdmm@gmail.com>
2022-05-02 21:05:57 +0500

    feat: completed count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,22 +1,27 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+//  * Given matrix where you have to find cats by ears "^^"
+//  *
+//  * @param {Array<Array>} matrix 
+//  * @return {Number} count of cats found
+//  *
+//  * @example
+//  * countCats([
+//  *  [0, 1, '^^'],
+//  *  [0, '^^', 2],
+//  *  ['^^', 1, 2]
+//  * ]) => 3`
+function countCats(arr) {
+  let count = 0;
+  const regexp = /^\^\^$/;
+
+  for(let item of arr) {
+    for(let elem of item) {
+      if(regexp.test(elem)) count++;
+    }
+  }
+  return count;
 }
 
 module.exports = {
