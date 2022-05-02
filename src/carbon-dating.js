6cce1e632d5c6d69699976ef35ee9d10fbbdadc1
davmmag <goodofdmm@gmail.com>
2022-05-03 01:05:32 +0500

    feat: finish

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") return false;
+  sampleActivity = +sampleActivity;
+  if(isNaN(sampleActivity)) return false
+  if(sampleActivity >= MODERN_ACTIVITY || sampleActivity === 0) return false;
+
+  const res = Math.ceil(Math.abs((Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / 5730)));
+
+  return isNaN(res) ? false : res;
 }
 
 module.exports = {
