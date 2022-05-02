08683ad460b20b4c7c7118dadaa2a73bee69ff84
davmmag <goodofdmm@gmail.com>
2022-05-02 22:20:53 +0500

    feat: completed dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -9,15 +9,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
+
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+  return members.filter(item => (typeof item === 'string'))
+                .map(item => item.trim()[0].toUpperCase())
+                .sort()
+                .join('');
 }
+
+// console.log(createDreamTeam([
+//         ['David Abram'],
+//         ['Robin Attfield'],
+//         'Thomas Berry',
+//         ['Paul R.Ehrlich'],
+//         'donna Haraway',
+//         ' BrIaN_gOodWiN  ',
+//         {
+//           0: 'Serenella Iovino'
+//         },
+//         'Erazim Kohak',
+//         '  val_plumwood',
+// ]), 'BDETV');
 
 module.exports = {
   createDreamTeam
 };
+// console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']))//'ADMM'
+// console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]))//'LOO'
+// console.log(createDreamTeam([
+//         '   William Alston ',
+//         ' Paul Benacerraf',
+//         '  Ross Cameron',
+//         '       Gilles Deleuze',
+//         '  Arda Denkel ',
+//         '  Michael Devitt',
+//         '  Kit Fine',
+//         ' Nelson Goodman',
+//         'David Kolb',
+//         '   Saul Kripke',
+//         '  Trenton Merricks',
+//         '  Jay Rosenberg',
+// ]));
